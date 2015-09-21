76b289933241ec1af16fb4c634ce58bc7c4f63c3
vagrant <vagrant@homestead>
2015-09-21 03:18:16 +0000

    add wecenter project

---
+++ system/config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+$config['charset'] = 'utf8';
+$config['prefix'] = 'aws_';
+$config['driver'] = 'MySQLi';
+$config['master'] = array (
+  'charset' => 'utf8',
+  'host' => 'localhost',
+  'username' => 'homestead',
+  'password' => 'secret',
+  'dbname' => 'momoxue_wecenter',
+);
+$config['slave'] = false;
---
+++ uc_client/data/cache/apps.php
@@ -0,0 +1,37 @@
+<?php
+$_CACHE['apps'] = array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'OTHER',
+    'name' => 'wecenter',
+    'url' => 'http://wecenter.app',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'synlogin' => '1',
+    'extra' => 
+    array (
+      'apppath' => '',
+    ),
+    'recvnote' => '1',
+  ),
+  2 => 
+  array (
+    'appid' => '2',
+    'type' => 'OTHER',
+    'name' => 'phpdisk',
+    'url' => 'http://phpdisk.app',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'synlogin' => '1',
+    'extra' => 
+    array (
+      'apppath' => '',
+    ),
+    'recvnote' => '1',
+  ),
+);
